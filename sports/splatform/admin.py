22de03ab91e2f0dfb8548108a4cea4feec882964
颜琪滨 <2535367985@qq.com>
2021-05-02 23:40:34 +0800

    Update admin.py

--- sports/splatform/admin.py
+++ sports/splatform/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Player, Match, Team
 
+admin.site.register(Team)
 admin.site.register(Player)
+admin.site.register(Match)
+
 # Register your models here.
