4682dcdd10290d7fa667bc07b15f9166676eeab4
颜琪滨 <2535367985@qq.com>
2021-05-02 21:50:55 +0800

    Update models.py
    修改了一下数据库，添加了比赛与队伍，队伍与队员之间的联系

--- sports/splatform/models.py
+++ sports/splatform/models.py
@@ -2,31 +2,39 @@ from django.db import models
 
 
 class Player(models.Model):
-    player_name = models.CharField(max_length=100)
-    player_team = models.CharField(max_length=100)
-    goal = models.IntegerField(default=0)
-    assist = models.IntegerField(default=0)
-    y_card = models.IntegerField(default=0)
-    r_card = models.IntegerField(default=0)
+    # 运动员模型
+    player_name = models.CharField(max_length=30) # 运动员名字
+    # player_team = models.CharField(max_length=50) # 运动员所在队伍
+    goal = models.IntegerField(default=0) # 总进球数
+    assist = models.IntegerField(default=0) # 总助攻数
+    y_card = models.IntegerField(default=0) # 黄牌数
+    r_card = models.IntegerField(default=0) # 红牌数
+
+    team = models.ForeignKey(Team, related_name='players', on_delete=models.CASCADE) # 定义外键，Team和Player是1:n
 
     def __str__(self):
         return self.player_name
 
 
 class Match(models.Model):
-    match_no = models.IntegerField(default=0)
-    h_team = models.CharField(max_length=100)
-    a_team = models.CharField(max_length=100)
-    h_goal = models.IntegerField(default=0)
-    a_goal = models.IntegerField(default=0)
+    match_id = models.IntegerField(default=0) # 比赛序号
+    h_team_id = models.ForeignKey(Team, related_name='h_team', on_delete=models.CASCADE) # 主队
+    a_team_id = models.ForeignKey(Team, related_name='a_team', on_delete=models.CASCADE) # 客队
+
+    h_score = models.IntegerField(default=0) # 主队进球数
+    a_score = models.IntegerField(default=0) # 客队进球数
+    
+    match_time = models.DateTimeField() # 比赛时间
+    match_loc = models.CharField(max_length=20) # 比赛地点
 
     def __str__(self):
         return self.match_no
 
 
 class Team(models.Model):
-    team_name = models.CharField(max_length=100)
-    group = models.CharField(max_length=100)
+    team_name = models.CharField(max_length=50) # 队伍名
+    coach = models.CharField(max_length=20) # 主教练
+    group = models.CharField(max_length=100) # 小组名
 
     def __str__(self):
         return self.team_name
