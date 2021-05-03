b3d47d66ff917d1b337ca0e72d062533a4c36e6b
颜琪滨 <2535367985@qq.com>
2021-05-03 16:24:19 +0800

    Update urls.py
    评论区的urls

--- sports/boards/urls.py
+++ sports/boards/urls.py
@@ -5,6 +5,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('add_book/', add_book, ),
-    path('show_books/', show_books, ),
+    path('', views.BoardListView.as_view(), name='home'),
+    path(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
+    path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
+    path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
+    path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='edit_post'),
 ]
