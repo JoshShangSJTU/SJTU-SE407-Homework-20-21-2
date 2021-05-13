04182897a2bbd501ac71fed36c7f351744586f71
颜琪滨 <2535367985@qq.com>
2021-05-13 21:02:18 +0800

    Update urls.py
    将board的接口信息加入路由：http://127.0.0.1:8000/talk/api/board/

--- sports/boards/urls.py
+++ sports/boards/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 from django.urls import path, re_path
+from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register('board', views.BoardViewSet)
 
 urlpatterns = [
     path('', views.BoardListView.as_view(), name='home_boards'),
@@ -11,4 +14,6 @@ urlpatterns = [
     re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
     re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
     re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='edit_post'),
+    
+    path('api/', include(router.urls))
 ]
