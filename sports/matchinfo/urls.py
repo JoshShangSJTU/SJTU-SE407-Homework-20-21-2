7bfd919eed880de3f9157d873b01a799ba276b64
陈晨 <stars_in_bright@sjtu.edu.cn>
2021-05-16 21:22:54 +0800

    Update

--- sports/matchinfo/urls.py
+++ sports/matchinfo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
+from . import views
 
 urlpatterns = [
 
     path('home',TemplateView.as_view(template_name='home_match.html')),
+    path('POST',views.POSTtest)
 
 ]
--- sports/matchinfo/views.py
+++ sports/matchinfo/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def POSTtest(request):
+    if request.method == 'POST':
+        return("收到POST请求")
+    else:
+        return("未收到POST请求")
