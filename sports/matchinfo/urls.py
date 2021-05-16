82fe0456a0aead01235e7c3ff449f44b8aadfbb5
陈晨 <stars_in_bright@sjtu.edu.cn>
2021-05-16 20:58:37 +0800

    Update urls

---
+++ sports/matchinfo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.generic.base import TemplateView
+
+
+urlpatterns = [
+
+    path('home',TemplateView.as_view(template_name='home_match.html')),
+
+]
--- sports/sports/urls.py
+++ sports/sports/urls.py
@@ -28,8 +28,9 @@ urlpatterns = [
     path('talk/', include(boards.urls)), # 讨论区页面
 
     path('api/mgr/', include('mgr.urls')),
+    path('matchinfo/',include('matchinfo.urls')), # 赛事信息页
 
-    path('home-match',TemplateView.as_view(template_name='home_match.html'))#赛事信息页
+    path('home-match',TemplateView.as_view(template_name='home_match.html'))
 
 
 ]
