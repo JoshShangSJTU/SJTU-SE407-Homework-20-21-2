7f7436d18c862dc98eb1fd01fe10d940652c446c
xiashen <245069160@qq.com>
2021-05-16 20:32:27 +0800

    视图函数编写

--- sports/splatform/views.py
+++ sports/splatform/views.py
@@ -1,11 +1,15 @@
 from django.views.generic import TemplateView
 from .models import Player
+from django.shortcuts import render
 
 class HomeView(TemplateView):
     template_name = "home.html"
 
-    def score_list(request):
-        players = Player.objects.order_by("-goal")
-        return players
+    def goal_list(request):
+        players = {}
+        players = Player.objects.order_by('-goal')
+        return render(request, 'home.html', players)
+
+    
 
 
