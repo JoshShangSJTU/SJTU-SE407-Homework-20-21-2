7111a82d486adda433d050c019a0542adb444275
陈晨 <stars_in_bright@sjtu.edu.cn>
2021-05-22 15:21:27 +0800

    Update views.py

--- sports/matchinfo/views.py
+++ sports/matchinfo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
 
@@ -7,3 +8,11 @@ def POSTtest(request):
         return("收到POST请求")
     else:
         return("未收到POST请求")
+
+def Login(request):
+    #输入用户名提交后返回"hello,用户名"
+    if request.method == "POST":
+        username = request.POST.get('username')
+        return HttpResponse("hello,"+username)
+    else:#初始登录时返回一个静态登录页面
+        return render_to_response('login.html')
