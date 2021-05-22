99750b1f7b4bd6a195eed3d595061e8923458e8a
xiashen <245069160@qq.com>
2021-05-22 16:38:00 +0800

    views 2.1
    更改传参类型为字典

--- sports/splatform/views.py
+++ sports/splatform/views.py
@@ -2,14 +2,16 @@ from django.http.response import HttpResponse, JsonResponse
 from django.views.generic import TemplateView
 from .models import Player
 from django.shortcuts import render
+from django.forms.models import model_to_dict  
 
 class HomeView(TemplateView):
     template_name = "home.html"
 
     def goal_list(request):
         results = Player.objects.order_by('-goal')
+        result_dict = model_to_dict(results)
 
-        return render(request, 'home.html', results)
+        return render(request, 'home.html', result_dict)
     
     
     
