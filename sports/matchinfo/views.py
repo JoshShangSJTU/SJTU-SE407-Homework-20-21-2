f8837799f4dd10aa6f89bb94c44a409a5c51b33f
陈晨 <stars_in_bright@sjtu.edu.cn>
2021-05-22 17:56:41 +0800

    Update views.py

--- sports/matchinfo/views.py
+++ sports/matchinfo/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 
+
 from splatform import models
 # Create your views here.
+
+'''
+查询语法：
+.all()  选择全部对象
+.filter()   筛选符合括号内筛选条件的对象
+    如：    models.Book.objects.filter(pk=5)
+    pk为primary key，代表数据库中的id
+
+.exclude() 筛选不符合括号内筛选条件的对象
+.get()      筛选对象，且只能返回一个对象，否则报错
+.order_by() 方法用于对查询结果进行排序
+    a、参数的字段名要加引号。
+    b、降序为在字段前面加个负号 -。
+        如：    models.Book.objects.order_by(-"price")
+
+count() 方法用于查询数据的数量，返回的数据是整数
+
+'''
 
 def Login(request):
     #输入用户名提交后返回"hello,用户名"
@@ -14,6 +33,14 @@ def Login(request):
         return render(request,'home.html')
 
 def QueryPlayers(request):
-    players = models.Player.objects.all() 
-    print(players,type(players)) # QuerySet类型，类似于list，访问 url 时数据显示在命令行窗口中。
+    package = []
+    #player_count = models.Player.objects.all().count() 
+
+   
+    for player in models.Player.objects.all():
+        exec('player{}={}'.format(player.pk, player.pk))
+        exec('print(player{})'.format(player.pk))
+        exec('package.append(player{})'.format(player.pk))
+        print(package)
+    
     return HttpResponse("<p>查找成功！</p>")
