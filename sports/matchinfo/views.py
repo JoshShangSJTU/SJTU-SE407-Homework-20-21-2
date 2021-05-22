dfddf0b886c54607f21ffb33e5eab581ad85c8f2
陈晨 <stars_in_bright@sjtu.edu.cn>
2021-05-22 15:48:18 +0800

    Update

--- sports/matchinfo/views.py
+++ sports/matchinfo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 def POSTtest(request):
@@ -9,10 +11,11 @@ def POSTtest(request):
     else:
         return("未收到POST请求")
 
+@csrf_exempt
 def Login(request):
     #输入用户名提交后返回"hello,用户名"
     if request.method == "POST":
         username = request.POST.get('username')
         return HttpResponse("hello,"+username)
     else:#初始登录时返回一个静态登录页面
-        return render_to_response('login.html')
+        return render(request,'home.html')
