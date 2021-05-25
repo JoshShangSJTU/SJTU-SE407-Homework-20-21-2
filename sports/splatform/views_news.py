a8470595b1db514a84929f8fd7a7a35bb604adcf
xiashen <245069160@qq.com>
2021-05-25 23:48:06 +0800

    news 2.0
    post值传过去了，但是出现了奇怪的错误：HTTP Status 500 – Internal Server Error

--- sports/splatform/views_news.py
+++ sports/splatform/views_news.py
@@ -1,8 +1,13 @@
 from .models import News
 from django.shortcuts import redirect 
+from django.forms.models import model_to_dict
+
+
 
 def news_list(request):
     if request.method == 'POST':
-        r_id = request.POST.get('num')
-        url = News.objects.filter(num = r_id).values()['url']
-    return redirect(url)
+        num1 = request.POST.get('num')
+        url = News.objects.filter(num = num1).first()
+        url_dict = model_to_dict(url)
+
+    return redirect(url_dict['url'])
