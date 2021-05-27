7e27c76e8e0c3e71496bb41acb80731ba26a8cdd
颜琪滨 <2535367985@qq.com>
2021-05-27 21:15:57 +0800

    Update serializers.py
    修改了时间格式

--- sports/boards/serializers.py
+++ sports/boards/serializers.py
@@ -1,8 +1,12 @@
 # 这是序列化文件
 from django.db.models import fields
 from django.forms.models import model_to_dict
-from .models import Board, Topic, Post
 from rest_framework import serializers
+
+from .models import Board, Topic, Post
+
+import time, datetime
+
 
 class BoardSerializer(serializers.ModelSerializer):
     '''
@@ -17,12 +21,14 @@ class TopicSerializer(serializers.ModelSerializer):
     Topic序列化
     '''
     replies = serializers.SerializerMethodField()
+    last_updated = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
     class Meta:
         model = Topic
         fields = ('id', 'subject', 'last_updated', 'board', 'starter', 'views', 'replies')
         
     def get_replies(self, obj):
         return obj.posts.count()
+
 
 class PostListSerializer(serializers.ModelSerializer):
     '''
