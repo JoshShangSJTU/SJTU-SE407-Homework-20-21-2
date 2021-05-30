7f1184fc209b14487b4622f12824d42577510dd2
陈晨 <stars_in_bright@sjtu.edu.cn>
2021-05-30 15:48:52 +0800

    Update

--- sports/matchinfo/urls.py
+++ sports/matchinfo/urls.py
@@ -4,10 +4,13 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-
+    # 页面路径
     path('home/',TemplateView.as_view(template_name='home_match.html')),
     path('home/login/', TemplateView.as_view(template_name='login.html')),
-    path('match-detail',TemplateView.as_view(template_name='match_detail.html')),
-    path('Query',views.Query)
+    path('home/match-detail',TemplateView.as_view(template_name='match_detail.html')),
+    path('Query',views.Query),
+
+    #api路径
+    path('homedata',views.Homedata)
 
 ]
--- sports/matchinfo/views.py
+++ sports/matchinfo/views.py
@@ -21,13 +21,28 @@ from splatform import models
 count() 方法用于查询数据的数量，返回的数据是整数
 
 '''
+def Homedata(request):
+    match_package = []
+    player_package = []
+    team_package = []
 
+    for match in models.Match.objects.all():
+        matchinfo={'id':match.pk,'match_no':match.match_no,'h_score':match.h_score,'a_score':match.a_score,'match_loc':match.match_loc,'a_team_name_id':match.a_team_name_id,'h_team_name_id':match.h_team_name_id,'match_time':match.match_time}
+        match_package.append(matchinfo)
+
+    for player in models.Player.objects.all():       
+        playerinfo={'id':player.pk,'name':player.player_name,'goal':player.goal,'assist':player.assist,'r_card':player.r_card,'team_id':player.team_id,'y_card':player.y_card}
+        player_package.append(playerinfo)
+
+    for team in models.Team.objects.all():       
+        teaminfo={'id':team.pk,'team_name':team.team_name,'coach':team.coach,'group':team.group,'point':team.point}
+        team_package.append(teaminfo)
+    return {'Match_package':match_package,'Player_package':player_package,'Team_package':team_package}
 
 def Query(request):
     match_package = []
     player_package = []
     team_package = []
-    #player_count = models.Player.objects.all().count() 
 
     for match in models.Match.objects.all():
         matchinfo={'id':match.pk,'match_no':match.match_no,'h_score':match.h_score,'a_score':match.a_score,'match_loc':match.match_loc,'a_team_name_id':match.a_team_name_id,'h_team_name_id':match.h_team_name_id,'match_time':match.match_time}
