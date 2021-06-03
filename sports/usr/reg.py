1dee3ffd42269ea5ea925c71c71abab21968c254
sssdanger <78289876+sssdanger@users.noreply.github.com>
2021-06-03 19:19:54 +0800

    注册更新

--- sports/usr/reg.py
+++ sports/usr/reg.py
@@ -5,19 +5,20 @@ from django.contrib.auth import authenticate, login
 def register(request):
 
     userName = request.POST.get('username')
-    userexist = authenticate(username=userName)
-    if userexist is not None:
-        return JsonResponse({'ret': 1, 'msg': '用户名已存在'})
+    #userexist = authenticate(username=userName)
+   #if userexist is not None:
+    #    return JsonResponse({'ret': 1, 'msg': '用户名已存在'})
     passWord = request.POST.get('password')
     passWord2 = request.POST.get('password2')
     Email = request.POST.get('email')
-    
-    if not passWord == passWord2:
-        return JsonResponse({'ret': 1, 'msg': '两次密码不一致'})
-    user = User.objects.create_user(username=userName, password=passWord, email=Email)
-    user.save()
-    userlogin = authenticate(username=userName, password=passWord)
-    login(request,userlogin)
-    request.session['usertype'] = 'usr'
     return {'ret': 0}
+
+    #if not passWord == passWord2:
+    #    return JsonResponse({'ret': 1, 'msg': '两次密码不一致'})
+    #user = User.objects.create_user(username=userName, password=passWord, email=Email)
+    #user.save()
+    #userlogin = authenticate(username=userName, password=passWord)
+    #login(request,userlogin)
+    #request.session['usertype'] = 'usr'
+    #return {'ret': 0}
 
